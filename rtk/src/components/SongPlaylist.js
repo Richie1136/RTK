335a8ee3db28ffcea6fb7a19dafc32fa56a60343
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-02-23 14:04:07 -0500

    Rendering out the songs and adding songs to the song playlist list

--- rtk/src/components/SongPlaylist.js
+++ rtk/src/components/SongPlaylist.js
@@ -1,13 +1,18 @@
 import { createRandomSong } from "../data";
+import { useDispatch, useSelector } from "react-redux";
+import { addSong } from '../store'
 
 function SongPlaylist() {
+  const dispatch = useDispatch()
   // To Do:
   // Get list of songs
-  const songPlaylist = [];
+  const songPlaylist = useSelector((state) => state.songs)
 
   const handleSongAdd = (song) => {
     // To Do:
     // Add song to list of songs
+
+    dispatch(addSong(song))
   };
   const handleSongRemove = (song) => {
     // To Do:
---
+++ rtk/src/store/index.js
@@ -0,0 +1,23 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+
+const songsSlice = createSlice({
+  name: 'song',
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload)
+    },
+    removeSong(state, action) {
+      //
+    }
+  }
+})
+
+export const store = configureStore({
+  reducer: {
+    songs: songsSlice.reducer
+  }
+})
+
+export const { addSong } = songsSlice.actions
